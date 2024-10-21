cfd0563d507434153a7e4f3d3b78571b94097a5e
ayeshakanwal7 <ayeshakhatree1@gmail.com>
2024-10-21 13:27:33 +0500

    first commit

---
+++ ch1to5.js
@@ -0,0 +1,102 @@
+// Alert
+alert("helow world!");
+
+alert("Error! please enter a valid password.")
+// line break in alert box
+alert("welcome to JS land \nhappy coding!")
+
+alert("Welcome to JS land");
+alert("happy coding");
+
+// variables for srtrings
+var username;
+var myname = "ayesha kanwal"
+var message = "hellow world"
+alert(message);
+var studentname = "ayesha";
+var studentage = '15yearsold';
+var studentcourse = 'mobile application development';
+alert(studentname);
+alert(studentage);
+alert(studentcourse);
+
+var pizza = "pizza \npizz\npiz\npi\np"
+alert(pizza);
+
+var email = "my email addres is "
+alert(email + "ayeshakhatree1@gmail.com");
+
+var book = "A smarter way to leran javascipt"
+alert("i'm trying to leran from " + book);
+
+
+var html = "yahh! i can write html content through js"
+document.write(html);
+
+var practice = "javascript";
+alert(practice);
+document.write(practice);
+
+// numbers
+var age = 21;
+alert("my age is " + age);
+var n = 14;
+alert('you,ve visited this page' + n + "times");
+
+var birthday = 2002
+document.write('your birth year is ' + birthday);
+
+var visitorsname = "ayesha kanwal";
+var producttitle = "t-shirt"
+var quantity = 21;
+document.write(visitorsname + " ordered" + quantity + producttitle + "from xyz clothing");
+
+var a = "$"
+var b = "_"
+var c = "n"
+var legal = a + b + c
+alert(legal);
+document.write('A heading states "rules for naming js variables can only be' + a + b + c + "for exmaple $name, _name, name");
+
+
+var num1 = 42;
+var num2 = 43;
+var add = num1 + num2;
+document.write("the sum of 42 and 43 is " + add);
+
+
+var num1 = 42;
+var num2 = 43;
+var multiply = num1 * num2;
+document.write("the multiply of 42 and 43 is " + multiply);
+
+var num1 = 42;
+var num2 = 43;
+var sub = num1 - num2;
+document.write("the multiply of 42 and 43 is " + sub);
+
+var num1 = 42;
+var num2 = 43;
+var divide = num1 / num2;
+document.write("the multiply of 42 and 43 is " + divide);
+
+var num1 = 42;
+var num2 = 43;
+var modulus = num1 % num2;
+document.write("the multiply of 42 and 43 is " + modulus);
+
+var num;
+document.write("value after variable declaration is " + num);
+num = 5;
+document.write("initial value is " + num + "<br>");
+var incre = ++num;
+document.write("increament after value is " + incre + "<br>");
+var sum = incre + 7;
+document.write('value after addition is ' + sum + "<br>");
+var decre = --sum;
+document.write('value after decrement is ' + decre + "<br>");
+var division = decre / 3;
+document.write('the reminder is ' + division + "<br>");
+
+
+
